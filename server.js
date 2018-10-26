67a8d424769ddbc5a672e5d8de8c7871c146fe79
DavidMcKenzie218 <davidmckenzie218@gmail.com>
2018-10-26 21:22:45 +0100

    Setup the filestructure and added the server.js and react dependencies

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+
+const COMMENTS_FILE = path.join(__dirname, 'comments.json');
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
+app.use(express.static('client/build'));
+
+
+const server = app.listen(3000, function () {
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
