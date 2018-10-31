deedcb6b520b463608e85b1f3d47554d7a3deb1a
DavidMcKenzie218 <davidmckenzie218@gmail.com>
2018-10-31 22:30:12 +0000

    Added the header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,11 @@
+const React = require('react');
+
+const Header = React.createClass({
+  render: function(){
+    return(
+      <div>
+        <h2>Header</h2>
+      </div>
+    )
+  }
+})
--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,14 +1,12 @@
 const React = require('react');
 
-//I am using the ES6 class declaration as I have never used this before and I am taking the opertunity
-//to learn how to use ES6 and React together.
 const HomePage = React.createClass({
   render: function(){
     return(
       <div className = "HomePage">
         <h2>Home Page</h2>
       </div>
-      );
+    );
   }
 })
 
