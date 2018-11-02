04015036c26f8bf2691ad5e649000d1179efd863
DavidMcKenzie218 <davidmckenzie218@gmail.com>
2018-11-02 01:09:37 +0000

    The body of the page is now rendered on the page

--- client/src/components/Body.jsx
+++ client/src/components/Body.jsx
@@ -1,10 +1,16 @@
 const React = require('react');
 
 const Body = React.createClass({
+
   render: function(){
+
+    let paragraphs = this.props.data.map(function(paragraph){
+      return <p>{paragraph.para}</p>
+    })
+
     return(
       <div>
-        <p>Body</p>
+        {paragraphs}
       </div>
     );
   }
--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -6,7 +6,7 @@ const HeaderButton = require('./HeaderButton.jsx');
 
 const HomePage = React.createClass({
   getInitialState: function(){
-    return {headers: [{id:1, title:'Home'}, {id:2, title:'Title 1'}, {id:3, title:'Title 2'}], body: [{p1:"This is paragraph One"}, {p2:"This is paragraph Two"}]};
+    return {headers: [{id:1, title:'Home'}, {id:2, title:'Title 1'}, {id:3, title:'Title 2'}], body: [{para:"This is paragraph One"}, {para:"This is paragraph Two"}]};
   },
 
   render: function(){
