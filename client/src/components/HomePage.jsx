e9ec11c50b24d22c61438b65c79abfd3a9333aff
DavidMcKenzie218 <davidmckenzie218@gmail.com>
2018-11-02 01:05:48 +0000

    Removed the bundle from the repo as it was missed from the git ignore

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -6,14 +6,14 @@ const HeaderButton = require('./HeaderButton.jsx');
 
 const HomePage = React.createClass({
   getInitialState: function(){
-    return {headers: [{id:1, title:'Home'}, {id:2, title:'Title 1'}, {id:3, title:'Title 2'}]};
+    return {headers: [{id:1, title:'Home'}, {id:2, title:'Title 1'}, {id:3, title:'Title 2'}], body: [{p1:"This is paragraph One"}, {p2:"This is paragraph Two"}]};
   },
 
   render: function(){
     return(
       <div className = "HomePage">
         <Header data={this.state.headers}/>
-        <Body/>
+        <Body data={this.state.body}/>
       </div>
     );
   }
