3b5cf18b1338931f48f648b18e2311e46ccbc028
DavidMcKenzie218 <davidmckenzie218@gmail.com>
2018-11-19 22:46:31 +0000

    Added the database query to the server and made sure that it is connecting

--- db/databaseQuery.js
+++ db/databaseQuery.js
@@ -4,4 +4,14 @@ const DatabaseQuery = function(){
   this.url = "mongodb://localhost:27017/test_david_mckenzie_db"
 }
 
+DatabaseQuery.prototype = {
+  all: function(){
+    MongoClient.connect(this.url, function(err, db){
+      if(db){
+        console.log("database connected");
+      }
+    })
+  }
+}
+
 module.exports = DatabaseQuery;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const fs = require('fs');
 
 const TestApi = require('./api/testApi.js');
+const DatabaseQuery = require('./db/databaseQuery.js');
 
 const COMMENTS_FILE = path.join(__dirname, 'comments.json');
 
@@ -19,6 +20,9 @@ const server = app.listen(3000, function () {
   const port = server.address().port;
 
   new TestApi(app);
+
+  let query = new DatabaseQuery();
+  query.all();
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
